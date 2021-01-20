cdfcb79a64406d876b0b800123354714c4ed14cc
Diego Torres <torressdiiego@outlook.com>
2021-01-20 09:37:37 -0300

    Inicio do projeto
    -Inicio do projeto Lumen 
    -Criação das models de Equipe e Campeonato

---
+++ api-campeonatobrasileiro/app/Providers/AppServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use app\Models\Equipe;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
